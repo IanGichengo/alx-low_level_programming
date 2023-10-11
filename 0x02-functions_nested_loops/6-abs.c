6950c47a7de391ff0acfa9d3c7ccb826d7b64235
IanGichengo <iangatimu6@gmail.com>
2023-10-11 15:41:29 -0400

    nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - returns value of an integer
+ * @n: prints n
+ * Return: 0 success
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		return (-n);
+	}
+}
