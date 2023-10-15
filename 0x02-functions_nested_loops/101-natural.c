ea010728751c73dd8f9831d04102f617583a0d5c
IanGichengo <iangatimu6@gmail.com>
2023-10-15 10:54:53 -0400

    nested_loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - it iterates over the natural numbers from 1 to 1023
+ * Return: result
+ */
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
