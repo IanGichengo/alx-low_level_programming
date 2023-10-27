397911d0bb3278b27f9c65e1217100c733189444
IanGichengo <iangatimu6@gmail.com>
2023-10-27 10:50:20 -0400

    neither_irony_nor_sarcasm

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- start
+ * @argc: argument
+ * @argv: array
+ * Return: result
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1] * atoi(argv[2]));
+			return (0);
+			}
