08cb532aadb034c5fe7cef12e97b55e45ca87318
IanGichengo <iangatimu6@gmail.com>
2023-11-17 15:24:04 -0500

    hello_holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+; File: hello.asm
+; A 64-bit program in assembly that prints Hello, Holberton, followed by a new line
+
+        global  main
+        extern  printf
+
+        section .data
+msg:    db      "Hello, Holberton", 10, 0 ; The message to print, 10 is newline, 0 is null terminator
+
+        section .text
+main:   ; The main function
+        ; Prepare the argument for printf
+        mov     rdi, msg        ; The format string (first argument)
+        xor     rax, rax        ; No floating point arguments (required by x86_64 ABI)
+
+        ; Call printf
+        call    printf
+
+        ; Exit the program
+        mov     rax, 60         ; The exit system call number
+        xor     rdi, rdi        ; The exit code (0)
+        syscall                 ; Invoke the kernel
