90046915626de5d6808c15965d3e67fa73363f5b
IanGichengo <iangatimu6@gmail.com>
2023-11-21 12:46:27 -0500

    safe_version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,29 +8,36 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *slow = head;
-	const listint_t *fast = head;
-	int loop = 0;
+	size_t nodes = 0;
+	const listint_t *slow_ptr, *fast_ptr;
+
+	if (head == NULL)
+		exit(98);
+
+	slow_ptr = head;
+	fast_ptr = head;
+
+	while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr  = fast_ptr->next->next;
+
+		if (slow_ptr == fast_ptr)
+		{
+			printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			return (nodes);
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+	}
 
 	while (head != NULL)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
-		count++;
 		head = head->next;
-		if (loop == 0)
-		{
-			if (slow == fast && count > 1)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				loop = 1;
-			}
-			slow = slow->next;
-			if (fast->next != NULL)
-				fast = fast->next->next;
-		}
-		if (loop == 1 && head == slow)
-			break;
+		nodes++;
 	}
-	return (count);
+	return (nodes);
 }
