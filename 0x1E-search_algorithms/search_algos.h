16a557fc8ad0060f8150fd6d970558eba224585a
IanGichengo <iangatimu6@gmail.com>
2024-05-08 16:05:17 -0400

    search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,13 @@
 #ifndef SEARCH_ALGORITHMS_H
 #define SEARCH_ALGORITHMS_H
 
+/* libraries */
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
